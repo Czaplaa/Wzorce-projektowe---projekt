9cfbda4928fbca646a3f9d4490a3255cc155d468
Czaplaa <56114056+Czaplaa@users.noreply.github.com>
2021-12-30 12:01:50 +0100

    Add files via upload

---
+++ Interfaces/IClaas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projekt.Interfaces
+{
+    internal interface IClaas
+    {
+        string name { get; set; }
+
+        List<object> students { get; set; }
+
+
+    }
+}
---
+++ Interfaces/IStudent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projekt.Interfaces
+{
+    internal interface IStudent
+    {
+        int Id { get; set; }
+
+        string FirstName { get; set; }
+
+        string LastName { get; set; }
+
+        List<int > Grades { get; set; }   
+
+        List <int> Presence { get; set; }
+    }
+}
